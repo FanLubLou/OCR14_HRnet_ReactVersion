2a77e8c657915b50e15e68e5574825bdcc201227
FanLubLou <jaclermont@hotmail.com>
2024-10-15 12:10:56 +0200

    installing 4 REACT libraries dedicated to replace 4 jQuery plugins

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Error() {
   return (
-    <div>Error</div>
+    <div className='wrapperError'>
+        <h1 className='Error404'>404</h1>
+        <p className='Error404Content'>The page you are requesting does not exist.</p>
+        <Link className='Error404Link' to='/'>Go back to home page</Link>
+    </div> 
   )
 }
